6a733843b3f58c4b8f1c632b059c5fa27aa4d452
Logan W <loganlwright@gmail.com>
2021-10-16 14:30:13 -0400

    Add more DOM logic

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 ////DOM Logic ////
 
-const body = document.querySelector("body");
-const winnerText = document.createElement("h1");
-body.appendChild(winnerText);
+const information = document.querySelector(".information");
+
+const currentRoundWinner = document.createElement("h1");
+const currentScore = document.createElement("h2");
+const gameEnded = document.createElement("h1");
+
+information.appendChild(currentRoundWinner);
+information.appendChild(currentScore);
+information.appendChild(gameEnded);
 
 /// Game Logic ////
 const choices = ["rock", "paper", "scissors"];
@@ -23,29 +29,29 @@ function playRound(playerSelection, computerSelection) {
   console.log(playerSelection, computerSelection);
   //if both pick the same index, tie
   if (playerSelection === computerSelection) {
-    console.log("Tie, no points scored.");
+    currentRoundWinner.innerText = "Tie, no points scored.";
   } else if (playerSelection > computerSelection) {
     if (playerSelection === 2 && computerSelection === 0) {
       // player === 2, computer === 0, computer must stll win
-      console.log("You lose, rock beats scissors.");
+      currentRoundWinner.innerText = "You lose, rock beats scissors.";
       computerScore++;
     } else if (playerSelection === 1) {
-      console.log("You win, paper beats rock!");
+      currentRoundWinner.innerText = "You win, paper beats rock!";
       playerScore++;
     } else {
-      console.log("You win, scissors beats paper!");
+      currentRoundWinner.innerText = "You win, scissors beats paper!";
       playerScore++;
     }
   } else {
     if (playerSelection === 0 && computerSelection === 2) {
       // player === 0, computer === 2, player must stll win
-      console.log("You Win, rock beats scissors.");
+      currentRoundWinner.innerText = "You Win, rock beats scissors.";
       playerScore++;
     } else if (computerSelection === 1) {
-      console.log("You lose, paper beats rock!");
+      currentRoundWinner.innerText = "You lose, paper beats rock!";
       computerScore++;
     } else {
-      console.log("You lose, scissors beats paper!");
+      currentRoundWinner.innerText = "You lose, scissors beats paper!";
       computerScore++;
     }
   }
@@ -61,11 +67,14 @@ const gameButtons = document.querySelectorAll(".btn");
 
 gameButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (computerScore === 5 || playerScore === 5) {
-      winnerText.innerText = `----GAME OVER----\n Player Score: ${playerScore}\n Computer Score: ${computerScore}`;
+    playRound(Number(button.id), (computerSelection = computerPlay()));
+    currentScore.innerText = `Current Score: Player Score: ${playerScore} Computer Score: ${computerScore}`;
+    if (playerScore == 5) {
+      gameEnded.innerText = `Game Over. Nice, You Win!\n Player Score: ${playerScore} Computer Score: ${computerScore}`;
       resetGame();
-    } else {
-      playRound(Number(button.id), (computerSelection = computerPlay()));
+    } else if (computerScore == 5) {
+      gameEnded.innerText = `Game Over. You lost!\n Player Score: ${playerScore} Computer Score: ${computerScore}`;
+      resetGame();
     }
   });
 });
